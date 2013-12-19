472a15688bb9e76491b17e5a8480e5f831808954
U-PS11-09\vantan <vantan@PS11-09.(none)>
2013-12-20 02:46:39 +0900

    version 1.0

--- calc3.c
+++ calc3.c
@@ -13,26 +13,35 @@ enum TokType
 int gettok(char *buf, int bufsiz)
 {
 	//ここに書いてください	
-	int c;
+	int c = 0;
 	int count = 0;
 	while((c = getchar()) != EOF)
 	{
+		// スペースだった場合はスキップ
 		if(isspace(c))
 		{
 			continue;
 		}
+		// 演算子だった場合の処理
 		if(c == '+' || c == '-' || c == '=' )
 		{
-			putchar(c);
+			buf[count] = c;
+			count++;
+			if(c == '+')
+				return OpPlus;
+			if(c == '-')
+				return OpMinus;
+			if(c == '=')
+				return OpEqual;
 			
 			break;
-		}else if(isdigit(c)){
+		}else if(isdigit(c)){ // １０進数の場合
 			do{
-				putchar(c);
 				buf[count] = c;
 				count++;
-				break;
-			}while(isdigit(c));
+				
+			}while((c = getchar()) && isdigit(c));
+			return Number;
 			
 		}else{
 			fprintf(stderr, "invalid char '%c'\n", c);
@@ -45,9 +54,13 @@ int main()
 {
 	enum TokType tok;
 	char buf[100];
-	
+	int i;
 	while((tok = gettok(buf, sizeof(buf))) != EndOfFile){
 		printf("tok = %d; buf = [%s]\n", tok, buf);
+		
+		for(i = 0; i < 100; i++){
+			buf[i] = 0;
+		}	
 	}
 	
 	return 0;
